33a7b994b1655ced1d6a19077894e5e0dead7783
Igor Sobral <igorgmz1421@gmail.com>
2023-12-15 16:36:09 -0300

    fix: AppRoutes private

--- src/pages/AppRoutes/index.tsx
+++ src/pages/AppRoutes/index.tsx
@@ -1,21 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes,Navigate  } from 'react-router-dom';
+import React from "react";
+import { Route, Routes, Navigate } from "react-router";
 
-import { Home } from '../home';
-import { CreateCampanha } from '../campanha/createCampanha';
-import { Login } from '../sign-in';
-import { EditCampanha } from '../campanha/editCampanha';
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { Home } from "../home";
+import { CreateCampanha } from "../campanha/createCampanha";
+import { Login } from "../sign-in";
+import { EditCampanha } from "../campanha/editCampanha";
+import { getLocalStorage } from "../../utils/local-storage";
 
 const AppRouter = () => {
+  const isAuthenticated = getLocalStorage();
+
   return (
     <Router>
       <Routes>
-        <Route path="/login"  element={<Login/>} />
-        <Route path="/"  element={<Navigate to={"/login"}/>} />
-        <Route path="/campanhas"  element={<Home/>} />
-        <Route path="/createcampanha" element={<CreateCampanha/>} />
-        <Route path="/editcampanha" element={<EditCampanha/>} />
-        
+        <Route path="/" element={<Navigate to="/campanhas" />} />
+        <Route
+          path="/login"
+          element={isAuthenticated ? <Navigate to="/campanhas" /> : <Login />}
+        />
+        <Route path="/campanhas" element={<Home />} />
+        <Route
+          path="/createcampanha"
+          element={
+            isAuthenticated ? <CreateCampanha /> : <Navigate to="/login" />
+          }
+        />
+        <Route
+          path="/editcampanha/:id"
+          element={
+            isAuthenticated ? <EditCampanha /> : <Navigate to="/login" />
+          }
+        />
       </Routes>
     </Router>
   );
