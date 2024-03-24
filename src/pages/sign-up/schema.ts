920103c70bf3062d6638363ca6fa1acfecf8ff22
Igor Sobral <igorgmz1421@gmail.com>
2024-03-24 19:44:09 -0300

    cchange validate schema

--- src/pages/sign-up/schema.ts
+++ src/pages/sign-up/schema.ts
@@ -1,24 +1,23 @@
 import { z } from "zod";
 
+//validações dos inputs utilizando a lib zod
 export const signUpSchema = z
   .object({
     name: z
       .string({ required_error: "Digite seu nome!" })
       .min(1, { message: "Digite um nome válido!" }),
-    cpf: z
+    CPF: z
       .string({
         required_error: "Campo não pode ser vazio!",
       })
       .min(1, { message: "Digite seu cpf" })
-      .regex(
-        /^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}$/,
-        { message: "CPF inválido" }
-      ),
-    phone: z
-      .string({
-        required_error: "Campo não pode ser vazio!",
-      })
-      .min(1, { message: "Digite seu Telefone" }),
+      .regex(/^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}$/, {
+        message: "CPF inválido",
+      }),
+    phoneNumber: z
+      .string({ required_error: "Campo não pode ser vazio!" })
+      .min(1, { message: "Digite seu Telefone" })
+      .regex(/^\d{11}$/, { message: "Telefone inválido" }),
     email: z
       .string({ required_error: "Digite um email!" })
       .min(1, { message: "Email obrigatório!" })
@@ -29,7 +28,7 @@ export const signUpSchema = z
       })
       .min(8, "deve conter no mínimo 8 characters")
       .max(32, "deve conter no máximo 32 characters"),
-    confirmPassword: z.string({required_error: "Repita a senha"}),
+    confirmPassword: z.string({ required_error: "Repita a senha" }),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "As senhas não coincidem",
