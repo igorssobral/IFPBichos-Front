f6c515f0d8644df8afeb37e2ed25f9b10f09c1e3
Igor Sobral <igorgmz1421@gmail.com>
2024-07-20 11:32:31 -0300

    chore: change schema create campaign

--- src/pages/campanha/createCampanha/schema.ts
+++ src/pages/campanha/createCampanha/schema.ts
@@ -1,52 +1,60 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const createCampaignSchema = z.object({
   title: z
-    .string({ required_error: "Adicione um título!", invalid_type_error: "" })
-    .min(3, { message: "Mínimo de 3 caracteres para o título" }),
+    .string({ required_error: 'Adicione um título!', invalid_type_error: '' })
+    .min(3, { message: 'Mínimo de 3 caracteres para o título' }),
 
   description: z
     .string({
-      required_error: "Campo não pode ser vazio!",
-      invalid_type_error: "",
+      required_error: 'Campo não pode ser vazio!',
+      invalid_type_error: '',
     })
-    .max(255, { message: "A descrição deve ter menos de 255 caracteres" })
-    .min(1, { message: "A descrição não pode estar vazia" }),
+    .max(255, { message: 'A descrição deve ter menos de 255 caracteres' })
+    .min(1, { message: 'A descrição não pode estar vazia' }),
   animal: z
     .string({
-      required_error: "Campo não pode ser vazio!",
-      invalid_type_error: "",
+      required_error: 'Campo não pode ser vazio!',
+      invalid_type_error: '',
     })
-    .min(1,{ message: "Selecione um animal" }),
+    .min(1, { message: 'Selecione um animal' }),
 
   fundraisingGoal: z.coerce
     .number({
-      required_error: "Campo não pode ser vazio!",
-      invalid_type_error: "Digite uma meta",
+      required_error: 'Campo não pode ser vazio!',
+      invalid_type_error: 'Digite uma meta',
     })
-    .positive({ message: "Digite uma meta válida" }),
+    .positive({ message: 'Digite uma meta válida' }),
 
   startDate: z.coerce
     .date({
-      required_error: "Escolha uma data!",
-      invalid_type_error: "Escolha uma data",
+      required_error: 'Escolha uma data!',
+      invalid_type_error: 'Escolha uma data',
     })
-    .refine((data) => data < new Date(), { message: "Data Inválida" }),
+    .refine((data) => data < new Date(), { message: 'Data Inválida' }),
   finishedDate: z.coerce
     .date({
-      required_error: "Escolha uma data!",
-      invalid_type_error: "Escolha uma data",
+      required_error: 'Escolha uma data!',
+      invalid_type_error: 'Escolha uma data',
     })
     .refine((data) => data > new Date(), {
-      message: "Data final precisa ser maior que a de inicio",
+      message: 'Data final precisa ser maior que a de inicio',
     }),
 
   file: z
-    .string({ required_error: "Selecione uma imagem!" })
-    .min(1, { message: "O arquivo é obrigatório" })
-    .refine((file) => file?.trim() !== "", {
-      message: "O arquivo é obrigatório",
+    .string({ required_error: 'Selecione uma imagem!' })
+    .min(1, { message: 'O arquivo é obrigatório' })
+    .refine((file) => file?.trim() !== '', {
+      message: 'O arquivo é obrigatório',
     }),
 });
 
-export type CreateCampaignSchema = z.infer<typeof createCampaignSchema>;
+export type CreateCampaignSchema = {
+  title: string;
+  fundraisingGoal: number;
+  description: string;
+  animal: number;
+  startDate: string;
+  finishedDate: string;
+  file: File | null;
+};
