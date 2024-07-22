b2c115c01857dec58acac539e36129b899b878f4
Igor Sobral <igorgmz1421@gmail.com>
2024-07-22 12:48:29 -0300

    feat: add lib toastify for alert messages

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import './App.css';
 import AppRouter from './pages/AppRoutes';
 import { AuthProvider } from './context/auth-context';
+import { Bounce, ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className='App'>
       <AuthProvider>
         <AppRouter />
+        <ToastContainer
+          position='top-right'
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme='light'
+          transition={Bounce}
+        />
       </AuthProvider>
     </div>
   );
