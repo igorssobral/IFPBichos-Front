96c882fc4bc42b20d0c85cd9a55db9900e14bdd5
igorssobral <igorgmz1421@gmail.com>
2024-12-28 22:44:35 -0300

    chore: move cloudinary file to services path

--- src/services/cloudinary.ts
+++ src/services/cloudinary.ts
@@ -1,3 +1,3 @@
-const cloudName = import.meta.env.VITE_APP_CLOUD_NAME; // Substitua pelo seu Cloud Name
+const cloudName = import.meta.env.VITE_APP_CLOUD_NAME; 
 
 export const CLOUDINARY_URL = `https://res.cloudinary.com/${cloudName}/image/upload/`;
